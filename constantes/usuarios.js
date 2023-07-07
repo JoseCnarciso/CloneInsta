67a7c26f0e234a129535dfbddfb0330c3cc82d9e
JoseCnarciso <josecdia@hotmail.com>
2023-07-06 22:32:11 -0300

    adicionando login e senha na classe usuarios

--- constantes/usuarios.js
+++ constantes/usuarios.js
@@ -1,6 +1,7 @@
 export const usuarios = [
     { email: 'henrique@gmail.com', password: 'devinhouse123' },
     { email: 'douglas@gmail.com', password: 'devinhouse321' },
-    { email: 'cavalcante@gmail.com', password: 'senha' }
+    { email: 'cavalcante@gmail.com', password: 'senha' },
+    {email:'josecdia@hotmail.com',password:'87120518'}
 ]
 
